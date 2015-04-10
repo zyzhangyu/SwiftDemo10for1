736bde530170537402f5dc038c11ddbba3f9c830
zyzhangyu <ios_devzhangyu@iOS-DevZhangYudeMacBook-Pro.local>
2015-04-10 09:18:40 +0800

    1.创建swift工程
    1.创建工程

---
+++ SwiftDemo10for1/SwiftDemo10for1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftDemo10for1
+//
+//  Created by iOS_DevZhangYu on 15/4/10.
+//  Copyright (c) 2015年 iOS_DevZhangYu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
