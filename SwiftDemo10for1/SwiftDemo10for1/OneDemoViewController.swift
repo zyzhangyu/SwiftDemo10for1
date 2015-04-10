8a8ca11bfd89c8a1ee2385ae2101e70239bdf7f0
zyzhangyu <ios_devzhangyu@iOS-DevZhangYudeMacBook-Pro.local>
2015-04-10 09:21:44 +0800

    1.创建demo1的文件夹 2.创建demo1的view controller

---
+++ SwiftDemo10for1/SwiftDemo10for1/OneDemoViewController.swift
@@ -0,0 +1,35 @@
+//
+//  OneDemoViewController.swift
+//  SwiftDemo10for1
+//
+//  Created by iOS_DevZhangYu on 15/4/10.
+//  Copyright (c) 2015年 iOS_DevZhangYu. All rights reserved.
+//
+
+import UIKit
+
+class OneDemoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
